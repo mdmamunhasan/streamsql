d11253a644e8937ecf8c48e8f2f96cf71706784d
mamun <mamunthecoder@gmail.com>
2017-05-14 23:20:37 +0600

    bash command update

--- consumer.sh
+++ consumer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 /usr/local/spark/bin/spark-submit --verbose --master local --class \
-com.awsapache.streamsql.MainSparkStreaming --jars \
+com.awsapache.streamsql.MainSparkStreaming \
 target/streamsql-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
 localhost:9092 topictest
