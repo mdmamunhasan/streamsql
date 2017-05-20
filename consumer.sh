869622f371d2038589d07d998a01a69831ffaeef
mamun <mamunthecoder@gmail.com>
2017-05-20 17:04:39 +0600

    redshift library add

--- consumer.sh
+++ consumer.sh
@@ -2,4 +2,4 @@
 /usr/local/spark/bin/spark-submit --verbose --master local --class \
 com.awsapache.streamsql.MainSparkStreaming \
 target/streamsql-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-localhost:9092 topictest jdbc s3n
+localhost:9092 topictest jdbc:redshift://52.77.123.232:5439/telenorhealth?user=admin&password=Telenor!23 s3n://redshift-temp-spark/data
